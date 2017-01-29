11d20b2461b139d15b2c27f45f5e715d56397c36
Ian Fraser <Ian Fraser>
2017-01-29 10:35:01 +0000

    Actually open a new tab

--- Open Text URL/background.js
+++ Open Text URL/background.js
@@ -23,15 +23,17 @@ chrome.extension.onMessage.addListener(function (request, sender, sendResponse) 
 function onRequest(info, tab) {
     if (info.linkUrl) {
         alert('link ' + info.linkUrl);
-        //        chrome.tabs.create({
-        //            url: serviceCall
-        //        });
+                chrome.tabs.create({
+                    url: info.linkUrl
+                });
         return;
     }
 
     if (info.selectionText) {
         var selection = info.selectionText;
         alert('selection ' + selection);
+
+        // Regex from http://stackoverflow.com/a/19696443/86148
         var urlRegex = /((?:(http|https|Http|Https|rtsp|Rtsp):\/\/(?:(?:[a-zA-Z0-9\$\-\_\.\+\!\*\'\(\)\,\;\?\&\=]|(?:\%[a-fA-F0-9]{2})){1,64}(?:\:(?:[a-zA-Z0-9\$\-\_\.\+\!\*\'\(\)\,\;\?\&\=]|(?:\%[a-fA-F0-9]{2})){1,25})?\@)?)?((?:(?:[a-zA-Z0-9][a-zA-Z0-9\-]{0,64}\.)+(?:(?:aero|arpa|asia|a[cdefgilmnoqrstuwxz])|(?:biz|b[abdefghijmnorstvwyz])|(?:cat|com|coop|c[acdfghiklmnoruvxyz])|d[ejkmoz]|(?:edu|e[cegrstu])|f[ijkmor]|(?:gov|g[abdefghilmnpqrstuwy])|h[kmnrtu]|(?:info|int|i[delmnoqrst])|(?:jobs|j[emop])|k[eghimnrwyz]|l[abcikrstuvy]|(?:mil|mobi|museum|m[acdghklmnopqrstuvwxyz])|(?:name|net|n[acefgilopruz])|(?:org|om)|(?:pro|p[aefghklmnrstwy])|qa|r[eouw]|s[abcdeghijklmnortuvyz]|(?:tel|travel|t[cdfghjklmnoprtvwz])|u[agkmsyz]|v[aceginu]|w[fs]|y[etu]|z[amw]))|(?:(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9])\.(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9]|0)\.(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9]|0)\.(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[0-9])))(?:\:\d{1,5})?)(\/(?:(?:[a-zA-Z0-9\;\/\?\:\@\&\=\#\~\-\.\+\!\*\'\(\)\,\_])|(?:\%[a-fA-F0-9]{2}))*)?(?:\b|$)/gi;;
         var theUrls = selection.match(urlRegex);
         if (theUrls.length == 0) {
@@ -39,14 +41,17 @@ function onRequest(info, tab) {
         }
         alert('theUrl ' + theUrls[0]);
         //do something with the selection
-        //var serviceCall = urls[0];
-        //chrome.tabs.create({
-        //    url: serviceCall
-        //});
+        var serviceCall = theUrls[0];
+        if (!serviceCall.startsWith("http")) {
+            serviceCall = 'http://' + serviceCall;
+        }
+
+        chrome.tabs.create({
+            url: serviceCall
+        });
     }
 };
 
-alert('load');
 chrome.contextMenus.create({
     id: "text_url_open",
     title: "Open in new tab......",
